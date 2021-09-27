eff0ddc2048a5007ea6d5d9e65b8f4e009eccfac
abellora <andrea.bellora@cern.ch>
2021-09-27 14:30:40 +0200

    First commit

---
+++ prescalePlotter/plugins/prescalePlotter.cc
@@ -0,0 +1,113 @@
+// -*- C++ -*-
+//
+// Package:    PPSTools/prescalePlotter
+// Class:      prescalePlotter
+//
+/**\class prescalePlotter prescalePlotter.cc PPSTools/prescalePlotter/plugins/prescalePlotter.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Andrea Bellora
+//         Created:  Tue, 21 Sep 2021 10:28:45 GMT
+//
+//
+
+// system include files
+#include <memory>
+#include <string>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/stream/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "HLTrigger/HLTcore/interface/HLTPrescaleProvider.h"
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<>
+// This will improve performance in multithreaded jobs.
+
+class prescalePlotter : public edm::stream::EDAnalyzer<> {
+public:
+  explicit prescalePlotter(const edm::ParameterSet&);
+  ~prescalePlotter() override;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+  void endRun(edm::Run const &, edm::EventSetup const &) override;
+  void beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup) override;
+  void analyze(const edm::Event&, const edm::EventSetup&) override;
+
+  // ----------member data ---------------------------
+  std::string processName_, triggerName_;
+  HLTPrescaleProvider hltPrescaleProvider_;
+};
+
+prescalePlotter::prescalePlotter(const edm::ParameterSet& iConfig)
+    : hltPrescaleProvider_(iConfig, consumesCollector(), *this) {
+  processName_ = iConfig.getParameter<std::string>("processName");
+  triggerName_ = iConfig.getParameter<std::string>("triggerName");
+}
+
+prescalePlotter::~prescalePlotter() = default;
+
+void prescalePlotter::endRun(edm::Run const &, edm::EventSetup const &) {}
+
+// ------------ method called for each event  ------------
+void prescalePlotter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  using namespace edm;
+
+  const std::pair<int, int> prescales(hltPrescaleProvider_.prescaleValues(iEvent, iSetup, triggerName_));
+  LogInfo("PPS") << "prescalePlotter::analyzeTrigger: path " << triggerName_ << " "
+                 << "prescales L1T,HLT: " << prescales.first << "," << prescales.second << endl;
+}
+
+void prescalePlotter::beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup) {
+  using namespace edm;
+  bool changed(true);
+  if (hltPrescaleProvider_.init(iRun, iSetup, processName_, changed)) {
+    HLTConfigProvider const& hltConfig = hltPrescaleProvider_.hltConfigProvider();
+    // if init returns TRUE, initialisation has succeeded!
+    if (changed) {
+      LogInfo("PPS") << "HLT configuration changed between runs";
+      const unsigned int n(hltConfig.size());
+      const unsigned int triggerIndex(hltConfig.triggerIndex(triggerName_));
+      if (triggerIndex >= n) {
+        LogInfo("PPS") << "prescalePlotter::analyze:"
+                       << " TriggerName " << triggerName_ << " not available in (new) config!" << endl;
+        LogInfo("PPS") << "Available TriggerNames are: " << endl;
+        hltConfig.dump("Triggers");
+      }
+    }
+  } else {
+    LogError("PPS") << " HLT config extraction failure with process name " << processName_;
+  }
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void prescalePlotter::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.add<std::string>("processName", "HLTX");
+  desc.add<std::string>("triggerName", "HLT_CTPPSFilter_v2");
+  desc.add<unsigned int>("stageL1Trigger", 1);
+  descriptions.add("prescalePlotter", desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(prescalePlotter);
---
+++ prescalePlotter/test/testConfig.py
@@ -0,0 +1,123 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process('TEST')
+#process = cms.Process('TEST', eras.Run2_$year, eras.run2_miniAOD_devel)
+
+from conditions import *
+
+def SetConditions(process):
+  # chose GT
+  process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+  process.GlobalTag = GlobalTag(process.GlobalTag, "120X_dataRun2_v2")
+
+  # chose LHCInfo
+  UseLHCInfoGT(process)
+  #UseLHCInfoLocal(process)
+  #UseLHCInfoDB(process, "frontier://FrontierProd/CMS_CONDITIONS", "LHCInfoEndFill_prompt_v2")
+
+  # chose alignment
+  UseAlignmentGT(process)
+  #UseAlignmentLocal(process)
+  #UseAlignmentFile(process, "sqlite_file:/afs/cern.ch/user/c/cmora/public/CTPPSDB/AlignmentSQlite/CTPPSRPRealAlignment_v13Jun19_v1.db", "PPSRPRealAlignment_v13Jun19")
+  #UseAlignmentDB(process, "frontier://FrontierProd/CMS_CONDITIONS", "CTPPSRPAlignment_real_offline_v7")
+
+  # chose optics
+  UseOpticsGT(process)
+  #UseOpticsLocal(process)
+  #UseOpticsFile(process, "sqlite_file:/afs/cern.ch/user/w/wcarvalh/public/CTPPS/optical_functions/PPSOpticalFunctions_2016-2018_v9.db", "PPSOpticalFunctions_test")
+  #UseOpticsDB(process, "frontier://FrontierProd/CMS_CONDITIONS", "PPSOpticalFunctions_offline_v6")
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+  statistics = cms.untracked.vstring(),
+  destinations = cms.untracked.vstring("cout"),
+  cout = cms.untracked.PSet(
+    threshold = cms.untracked.string("WARNING")
+  )
+)
+
+# raw data source
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring("file:/afs/cern.ch/work/m/maaraujo/public/output/output_Cal.root"),
+
+  # inputCommands = cms.untracked.vstring(
+  #   'drop *',
+  #   'keep FEDRawDataCollection_*_*_*'
+  # )
+
+  inputCommands = cms.untracked.vstring(
+    'keep *'
+  )
+  
+)
+
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32(-1)
+)
+
+# load default alignment settings
+process.load("CalibPPS.ESProducers.ctppsAlignment_cff")
+
+# raw-to-digi conversion
+process.load("EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff")
+
+# local RP reconstruction chain with standard settings
+process.load("RecoPPS.Configuration.recoCTPPS_cff")
+
+# define conditions
+SetConditions(process)
+CheckConditions()
+
+# reconstruction plotter
+process.ctppsProtonReconstructionPlotter = cms.EDAnalyzer("CTPPSProtonReconstructionPlotter",
+  tagTracks = cms.InputTag("ctppsLocalTrackLiteProducer"),
+  tagRecoProtonsSingleRP = cms.InputTag("ctppsProtons", "singleRP"),
+  tagRecoProtonsMultiRP = cms.InputTag("ctppsProtons", "multiRP"),
+
+  rpId_45_F = cms.uint32(23),
+  rpId_45_N = cms.uint32(3),
+  rpId_56_N = cms.uint32(103),
+  rpId_56_F = cms.uint32(123),
+
+  association_cuts_45 = process.ctppsProtons.association_cuts_45,
+  association_cuts_56 = process.ctppsProtons.association_cuts_56,
+
+  outputFile = cms.string("testMarianaOutput.root")
+)
+
+## load DQM framework
+process.load("DQM.Integration.config.environment_cfi")
+process.dqmEnv.subSystemFolder = "CTPPS"
+process.dqmEnv.eventInfoFolder = "EventInfo"
+process.dqmSaver.path = ""
+process.dqmSaver.tag = "CTPPS"
+
+## CTPPS DQM modules
+process.load("DQM.CTPPS.ctppsDQM_cff")
+
+# Modify to match Mariana's product name
+process.ctppsPixelDigis.inputLabel = cms.InputTag("hltPPSCalibrationRaw","","HLTX")
+
+# Comply with events not passing the HLT not having the RAW data
+process.options = cms.PSet(
+  SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
+
+# Prescale plotter
+process.load("PPSTools.prescalePlotter.prescalePlotter_cfi")
+
+# processing sequences
+process.path = cms.Path(
+  process.ctppsRawToDigi
+  * process.recoCTPPS
+  * process.ctppsProtonReconstructionPlotter
+  * process.prescalePlotter
+  * process.ctppsDQMOfflineSource
+  * process.ctppsDQMOfflineHarvest
+)
+
+process.end_path = cms.EndPath(
+  process.dqmEnv +
+  process.dqmSaver
+)
